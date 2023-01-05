c6b46d7e97ae2d298895f1355128d899c49c642b
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-01-05 12:48:18 +0900

    feat: Add Vector2

---
+++ src/vector.rs
@@ -0,0 +1,30 @@
+use std::ops::Add;
+use std::ops::Mul;
+use std::ops::Sub;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Vector2(pub f64, pub f64);
+
+impl Add for Vector2 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl Sub for Vector2 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self(self.0 - other.0, self.1 - other.1)
+    }
+}
+
+impl Mul for Vector2 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self(self.0 * other.0, self.1 * other.1)
+    }
+}
