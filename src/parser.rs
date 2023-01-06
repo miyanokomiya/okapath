ee7ea2e2e21b6cc9186a14e6dcbbdb44acdff253
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-01-06 22:22:20 +0900

    feat: Add parser to drop whitespace

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use crate::path::PathSegment;
 
 type ParserFn<'a> = fn(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)>;
-static PARSER_FN: [ParserFn; 3] = [parse_space, parse_number, parse_command];
+static PARSER_FN: [ParserFn; 2] = [parse_number, parse_command];
 
 pub fn parse(d: &str) -> Vec<PathSegment> {
     vec![]
@@ -13,10 +13,11 @@ fn split(d: &str) -> Vec<&str> {
     let mut cursor = 0;
     let mut ret: Vec<&str> = vec![];
 
+    cursor += drop_whitespace(&text, cursor);
     while cursor < len {
         let mut hit = false;
+
         for parser_fn in PARSER_FN {
-            println!("{:?} {:?}", text, cursor);
             hit = match parser_fn(&text, cursor) {
                 Some((value, size)) => {
                     cursor += size;
@@ -32,13 +33,29 @@ fn split(d: &str) -> Vec<&str> {
         if !hit {
             panic!("Invalid path.");
         }
+        cursor += drop_whitespace(&text, cursor);
     }
     ret
 }
 
-fn parse_space<'a>(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)> {
-    let mut value: Vec<char> = vec![];
-    None
+fn drop_whitespace(text: &Vec<char>, index: usize) -> usize {
+    let len = text.len();
+    let mut cursor = index;
+
+    while cursor < len {
+        match text.get(cursor) {
+            Some(' ') => {
+                cursor += 1;
+            }
+            Some(',') => {
+                cursor += 1;
+            }
+            _ => {
+                break;
+            }
+        }
+    }
+    cursor - index
 }
 
 fn parse_number<'a>(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)> {
@@ -47,7 +64,6 @@ fn parse_number<'a>(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)> 
 }
 
 fn parse_command<'a>(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)> {
-    println!("{:?} {:?}", text, index);
     match text.get(index) {
         Some('M') => Some(("M", 1)),
         _ => None,
@@ -59,8 +75,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn split_1() {
-        assert_eq!(split("M"), vec!["M"]);
-        assert_eq!(split("MM"), vec!["M", "M"]);
+    fn split_drop_whitespace() {
+        assert_eq!(split("M M"), vec!["M", "M"]);
+        assert_eq!(split("M  M"), vec!["M", "M"]);
+        assert_eq!(split("M,M"), vec!["M", "M"]);
+        assert_eq!(split("M,,M"), vec!["M", "M"]);
+        assert_eq!(split(" M, ,M "), vec!["M", "M"]);
     }
 }
