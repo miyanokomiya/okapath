66e2b6bde4e5cb07a42038e3d85547f136798c34
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-01-06 20:19:03 +0900

    Add split function

--- src/parser.rs
+++ src/parser.rs
@@ -1,58 +1,66 @@
 use crate::path::PathSegment;
 
-type ParserFn = fn(text: &str, index: usize) -> Option<(&str, usize)>;
-static A: [ParserFn; 2] = [parse_space, parse_number];
+type ParserFn<'a> = fn(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)>;
+static PARSER_FN: [ParserFn; 3] = [parse_space, parse_number, parse_command];
 
 pub fn parse(d: &str) -> Vec<PathSegment> {
-    let len = d.len();
-    let mut cursor = 0;
-
-    while cursor < len {
-        for parser_fn in A {
-            match parser_fn(d, cursor) {
-                Some((text, size)) => {
-                    cursor += size;
-                    break;
-                }
-                None => {}
-            }
-            cursor = len;
-        }
-    }
     vec![]
 }
 
-fn parse_space(text: &str, index: usize) -> Option<(&str, usize)> {
+fn split(d: &str) -> Vec<&str> {
+    let text: Vec<char> = d.chars().collect();
+    let len = text.len();
+    let mut cursor = 0;
+    let mut ret: Vec<&str> = vec![];
+
+    while cursor < len {
+        let mut hit = false;
+        for parser_fn in PARSER_FN {
+            println!("{:?} {:?}", text, cursor);
+            hit = match parser_fn(&text, cursor) {
+                Some((value, size)) => {
+                    cursor += size;
+                    ret.push(value.clone());
+                    true
+                }
+                None => false,
+            };
+            if hit {
+                break;
+            }
+        }
+        if !hit {
+            panic!("Invalid path.");
+        }
+    }
+    ret
+}
+
+fn parse_space<'a>(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)> {
     let mut value: Vec<char> = vec![];
     None
 }
 
-fn parse_number(text: &str, index: usize) -> Option<(&str, usize)> {
+fn parse_number<'a>(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)> {
     let mut value: Vec<char> = vec![];
     None
 }
 
-// struct Scanner {
-//     cursor: usize,
-//     characters: Vec<char>,
-// }
-//
-// impl Scanner {
-//     fn new(text: &str) -> Self {
-//         Self {
-//             cursor: 0,
-//             characters: text.chars().collect(),
-//         }
-//     }
-//
-//     fn finished(self) -> bool {
-//         self.cursor >= self.characters.len()
-//     }
-//
-//     fn step(&mut self) -> Option<&char> {
-//         let ret = self.characters.get(self.cursor);
-//         self.cursor += 1;
-//         ret
-//     }
-// }
-//
+fn parse_command<'a>(text: &Vec<char>, index: usize) -> Option<(&'a str, usize)> {
+    println!("{:?} {:?}", text, index);
+    match text.get(index) {
+        Some('M') => Some(("M", 1)),
+        _ => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn split_1() {
+        assert_eq!(split("M"), vec!["M"]);
+        assert_eq!(split("MM"), vec!["M", "M"]);
+    }
+}
