b5411821d2c9d5aa2c75e75343559f1e92f69b29
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-01-09 20:02:50 +0900

    feat: Add interface for `getPointAtLength`
    - Add `js-sys` to return float array

--- src/lib.rs
+++ src/lib.rs
@@ -16,14 +16,14 @@ extern "C" {
     fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, okapath!");
+#[wasm_bindgen(js_name = getTotalLeng)]
+pub fn get_total_leng(d: &str) -> f64 {
+    path::get_path_length(&parser::parse(d))
 }
 
-#[wasm_bindgen]
-pub fn getPathLength(d: &str) -> f64 {
-    path::get_path_length(&parser::parse(d))
+#[wasm_bindgen(js_name = getPointAtLength)]
+pub fn get_point_at_length(d: &str, distance: f64) -> js_sys::Float64Array {
+    js_sys::Float64Array::from(&vec![0.0, 0.0][..])
 }
 
 #[cfg(test)]
@@ -32,7 +32,7 @@ mod tests {
 
     #[test]
     fn get_path_length_cases() {
-        assert_eq!(getPathLength("M10 10 L 40 10 L40 50"), 70.0);
-        assert_eq!(getPathLength("M10 10 L 40 10 L40 50z"), 120.0);
+        assert_eq!(get_total_leng("M10 10 L 40 10 L40 50"), 70.0);
+        assert_eq!(get_total_leng("M10 10 L 40 10 L40 50z"), 120.0);
     }
 }
